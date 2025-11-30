031e7098b0b8d54b721a40ed63203d938e23d92b
John Brunson <JohnBrunson2009@gmail.com>
2025-11-30 13:16:19 -0800

    Player state machine started

--- player/scripts/player.gd
+++ player/scripts/player.gd
@@ -1,18 +1,75 @@
 class_name Player extends CharacterBody2D
 
-func _process(_delta: float) -> void:
+#region /// State Machine Variables
+var states: Array [ PlayerState ]
+var current_state : PlayerState : 
+	get : return states.front()
+var previous_state : PlayerState :
+	get : return states [ 1 ]
 	
+#endregion
+
+#region /// standard variables
+var direction : Vector2 = Vector2 (0, 0)
+var gravity : float = 980
+
+#endregion
+
+func _ready() -> void:
+	#Initialize the states
+	initialize_states()
+	pass
+func _unhandled_input(event: InputEvent) -> void:
+	change_state(current_state.handle_input(event))
+	pass
+func _process(_delta: float) -> void:
+	update_direction()
+	change_state( current_state.process(_delta))
 	pass
 	
 func _physics_process(_delta: float) -> void:
-	velocity.x = 0.0
-	if Input.is_action_pressed("left"):
-		velocity.x = -100
-	elif Input.is_action_pressed("right"):
-		velocity.x = 100
-	velocity.y = velocity.y + 980 * _delta
-	# This does not work, was merely an experiment. Can be deleted.
-	if Input.is_action_pressed("jump"):
-		velocity.y = 1000
 	move_and_slide()
+	velocity.y += gravity * _delta
+	change_state( current_state.physics_process(_delta))
 	pass
+
+func initialize_states() -> void:
+	states = []
+	#gather all states
+	for c in $States.get_children():
+		if c is PlayerState:
+			states.append( c )
+			c.player = self
+	# failsafe
+	if states.size() == 0:
+		return
+	#initialize all states
+	for state in states:
+		state.init()
+		
+		change_state( current_state )
+		current_state.enter() 
+	#set first state
+	pass
+	print (states)
+
+func change_state( new_state : PlayerState ) -> void:
+	# Safety check
+	if new_state == null:
+		return
+	elif new_state == current_state:
+		return
+	# Check if current state is true, then call its exit function
+	if current_state:
+		current_state.exit()
+	# Then set the new state
+	states.push_back(new_state)
+	current_state.enter()
+	# Cleanup - Keep only the last three actions.
+	states.resize( 3 )
+	pass
+func update_direction() -> void:
+	#var prev_direction : Vector2 = direction
+	direction = Input.get_vector("left", "right", "up", "down")
+	pass
+	
